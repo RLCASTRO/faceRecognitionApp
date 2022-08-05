a4136fefbc731b37b095e6fef4033db05b63c29d
Rodrigo Castro <castro.rl@gmail.com>
2022-08-05 20:01:53 -0300

    stable version

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
+// import Particles from 'react-particles-js';
+// import tsParticles from 'tsparticles-engine'
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 
-
+// just to make the code clean, we separate the params object
+// const particleOptions = {
+//   particles: {
+//     line_linked: {
+//       shadow: {
+//         enable: true,
+//         color: "#3CA9D1",
+//         blur: 5
+//       }
+//     }
+//   }
+// }
 
 function App() {
   return (
     <div className="App">
+      {/* <tsParticles params={particleOptions} /> */}
       <Navigation />
       <Logo />
       <Rank />
