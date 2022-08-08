187f07908225f7c1d1a9b948945f241258685a25
Rodrigo Castro <castro.rl@gmail.com>
2022-08-07 23:27:52 -0300

    starting the clarifai implementation, everything is working so far. installed the package, imported the library, instatiate the object, configured the model key and the api key, the api is returning something so far.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import Particles from './components/Particles/Particles';
-// import Particles from 'react-particles-js';
-// import tsParticles from 'tsparticles-engine'
+import Clarifai from 'clarifai';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+
+const app = new Clarifai.App({
+  // this is your key found in the clarifai portal over the API keys
+ apiKey: '865481b8696842178603f181dfd59d79'
+});
 
 class App extends Component {
   constructor() {
@@ -23,6 +27,16 @@ class App extends Component {
 
   onButtonSubmit = () => {
     console.log('click');
+    // here the first parameter is the key related to the face FACE_DETECT_MODEL, you can find it by clicking over the 'clarifai' library import
+    app.models.predict("d02b4508df58432fbb84e800597b8959", "https://samples.clarifai.com/face-det.jpg").then(
+      function(response) {
+        console.log(response);
+      },
+      function(err) {
+
+      }
+
+    )
   }
 
 
